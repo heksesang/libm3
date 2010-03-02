fb7bc73eec1d981393596b50c5ccbd18f84ec3e4
dot.qwerty <dot.qwerty@1d506776-2081-11df-970b-617f64f7e5c1>
2010-03-02 01:40:11 +0000

    Added license information and README for ObjConv.

---
+++ src/ObjConv/README.txt
@@ -0,0 +1,31 @@
+ObjConv README
+
+ObjConv converts .m3 files used by Starcraft II to the OBJ model format.
+
+Features:
+- Parses vertices, normals, faces and uv maps.
+- Supports batch-conversion.
+
+Usage:
+ObjConv <path>
+
+
+--------------------------------------------------------------------
+
+
+ObjConv - M3 to OBJ converter
+Copyright (C) <year>  <name of author>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
--- src/ObjConv/main.cpp
+++ src/ObjConv/main.cpp
@@ -1,14 +1,24 @@
-/************************************************************************
-*                                                                       *
-*   Application: ObjConv                                                *
-*   Description: Converts .m3 files into .obj files.                    *
-*                PS: .obj files cannot hold animations                  *
-*                                                                       *
-*   Usage: ObjConv <filename>                                           *
-*                                                                       *
-*   Author: Gunnar Lilleaasen                                           *
-*                                                                       *
-************************************************************************/
+/****************************************************************************
+*                                                                           *
+*   ObjConv - M3 to OBJ converter                                           *
+*   Copyright (C) 2010  Gunnar Lilleaasen                                   *
+*                                                                           *
+*   This program is free software; you can redistribute it and/or modify    *
+*   it under the terms of the GNU General Public License as published by    *
+*   the Free Software Foundation; either version 2 of the License, or       *
+*   (at your option) any later version.                                     *
+*                                                                           *
+*   This program is distributed in the hope that it will be useful,         *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+*   GNU General Public License for more details.                            *
+*                                                                           *
+*   You should have received a copy of the GNU General Public License along *
+*   with this program; if not, write to the Free Software Foundation, Inc., *
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.             *
+*                                                                           *
+****************************************************************************/
+
 #include <iostream>
 #include "../model.h"
 
